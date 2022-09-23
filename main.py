a91d9a95673006dbd34e60eae865a4763d4fe0fa
lipinsp3 <piotr.lipinski.pl1@contractors.roche.com>
2022-09-23 16:49:41 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 
-
+import docker
 
 
 # This is a sample Python script.
@@ -17,12 +17,14 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # print_hi('PyCharm')
+    #
+    # command_list = ['checkfolder']
+    #
+    # command = 'checkfolder' #sys.argv[1]
 
-    command_list = ['checkfolder']
-
-    command = 'checkfolder' #sys.argv[1]
 
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
